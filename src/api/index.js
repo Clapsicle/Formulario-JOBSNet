799813022da3e23bdf5bf69ccd24bb0b4744a300
Clapsicle <clapsicle@gmail.com>
2021-08-29 14:13:10 -0300

    ajusta porta

--- src/api/index.js
+++ src/api/index.js
@@ -20,6 +20,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use(routes);
-app.listen("7000", () => {
-  console.log("rodando na porta 7000");
+app.listen("80", () => {
+  console.log("rodando na porta 80");
 });
